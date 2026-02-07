e098cc691e863d5f2afdb5a8127935b5b9689734
Nikilesh07 <nikilesh698@gmail.com>
2026-02-07 11:16:58 +0530

    Add files via upload

---
+++ niki python .py
@@ -0,0 +1,45 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+=============================== RESTART: C:/Users/DELL/OneDrive/Documents/mini project.py ===============================
+
+====== LIBRARY MENU ======
+1. Add Book
+2. View Books
+3. Issue Book
+4. Return Book
+5. Exit
+Enter your choice: 1
+How many books to add? 2
+Enter book name: java
+Enter book name: python
+Books added successfully ✅
+
+====== LIBRARY MENU ======
+1. Add Book
+2. View Books
+3. Issue Book
+4. Return Book
+5. Exit
+Enter your choice: 3
+Enter book name to issue: python
+Book issued successfully 
+
+====== LIBRARY MENU ======
+1. Add Book
+2. View Books
+3. Issue Book
+4. Return Book
+5. Exit
+Enter your choice: 4
+Enter book name to return: java
+Book was not issued 
+
+====== LIBRARY MENU ======
+1. Add Book
+2. View Books
+3. Issue Book
+4. Return Book
+5. Exit
+Enter your choice: 5
+Thank you for using Library System 
